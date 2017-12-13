ba31ebcfd97e25f6ff22b919bee610fd93716384
devendervb12 <info@s-max.in>
2017-12-13 08:42:01 +0530

    initial commit

---
+++ ZSPLITAPP/WebContent/zsplitapp/SPLIT.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("zsplitapp.SPLIT", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf zsplitapp.SPLIT
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf zsplitapp.SPLIT
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf zsplitapp.SPLIT
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf zsplitapp.SPLIT
+*/
+//	onExit: function() {
+//
+//	}
+
+});
